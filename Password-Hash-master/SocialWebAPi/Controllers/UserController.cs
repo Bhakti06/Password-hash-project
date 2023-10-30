9c1d1fe467055574abf84e3b8b8ad6b672414ce1
Bhakti06 <63954393+Bhakti06@users.noreply.github.com>
2023-10-30 22:31:37 +0530

    Add files via upload

---
+++ Password-Hash-master/SocialWebAPi/Controllers/UserController.cs
@@ -0,0 +1,38 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using SocialWebAPi.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace SocialWebAPi.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    
+    public class UserController : ControllerBase
+    {
+        private SocialDBContext _context;
+        public UserController(SocialDBContext context)
+        {
+            _context = context;
+        }
+
+        [HttpGet]
+        public ActionResult<IEnumerable<User>> GetUsers()
+        {
+            var users = _context.Users.ToList();
+
+            return users;
+        }
+
+        [HttpGet("{id}")]
+        public ActionResult<User> GetUser(int id)
+        {
+            var user = _context.Users.Find(id);
+            return user;
+
+        }
+
+    }
+}
